30d24d43245f1ee7c6ab58d2254f850d9b9bc800
Kyle Hovey <kyle.hovey@my.simpson.edu>
2016-11-01 12:48:59 -0500

    Navigation.php edit to try and fix errors

--- resources/navigation.php
+++ resources/navigation.php
@@ -1,12 +1,12 @@
 <!--Navigation-->
     <nav>
       <ul>
-        <li> <a href="../public_html/index.php">Main Page</a> </li>
-        <li> <a href="../public_html/football.php">Football</a> </li>
-        <li> <a href="../public_html/basketball.php">Basketball</a> </li>         
-        <li> <a href="../public_html/baseball.php">Baseball</a> </li>          
-        <li> <a href="../public_html/articles.php">Articles of the Day</a> </li>
-        <li> <a href="../public_html/about.php">About</a> </li>       
+        <li> <a href="index.php">Main Page</a> </li>
+        <li> <a href="football.php">Football</a> </li>
+        <li> <a href="basketball.php">Basketball</a> </li>         
+        <li> <a href="baseball.php">Baseball</a> </li>          
+        <li> <a href="articles.php">Articles of the Day</a> </li>
+        <li> <a href="about.php">About</a> </li>       
         <li style="float:right"> <a class="active" href="signup.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a> </li>
         <li style="float:right"> <a class="active" href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a> </li>
       </ul>
